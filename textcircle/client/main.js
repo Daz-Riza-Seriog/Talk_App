d0bd295399ebea78f0e3f03949916ad6368c0395
Sergio Andrés Díaz Ariza <69307962+CHEJIN95@users.noreply.github.com>
2020-12-06 19:00:59 -0500

    Clone-Mirror, Viewer-Iframe, Date-display

--- textcircle/client/main.js
+++ textcircle/client/main.js
@@ -2,11 +2,27 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import {Mongo} from 'meteor/mongo';
 import { ShareJS } from 'meteor/edemaine:sharejs';
+import {Session} from 'meteor/session'
 
 this.Documents = new Mongo.Collection('documnts');
 
 import './main.html';
 
+var myvar= 10;
+//update the session current_date
+//variable every 1000 miliseconds
+Meteor.setInterval(function(){
+  Session.set('current_date', new Date());
+},1000);
+
+Template.date_display.helpers({
+  current_date:function(){
+    return Session.get('current_date');
+  },
+  myvar:function () {
+    return myvar;
+  }
+});
 
 Template.editor.helpers({
   docid:function(){
@@ -16,6 +32,14 @@ Template.editor.helpers({
     }
     else {
       return undefined;
+    }
+  },
+  config:function(){
+    return function (editor) {
+      editor.on('change',function(cm_editor,info){
+        console.log(cm_editor.getValue());
+        $('#viewer_iframe').contents().find('html').html(cm_editor.getValue());
+      });
     }
   }
 });
--- textcircle/server/main.js
+++ textcircle/server/main.js
@@ -6,7 +6,7 @@ this.Documents = new Mongo.Collection('documnts');
 
 Meteor.startup(() => {
   if (!Documents.findOne()) { //falsey statement--if isn't there
-    Documents.insert({title:'my nw documnt'})
+    Documents.insert({title:'my new documnt'})
   }
   // code to run on server at startup
 });
